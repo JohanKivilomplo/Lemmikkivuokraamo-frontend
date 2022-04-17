74f8357fbafffa549dd25901b1d9f35283777f37
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2022-04-17 11:48:54 +0300

    lisätty oheistuotteille oma css ja napit

--- lemminkäinen/oheistuotteet.php
+++ lemminkäinen/oheistuotteet.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="Portfolio.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="oheistuotteet.css">    
+
     <title>LEMMINKAINEN</title>
 </head>
 http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
@@ -16,7 +17,7 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
   <header>
       <ul class="nav nav-tabs">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../lemminkäinen/index.php">ETUSIVU</a>
+          <a class="nav-link" aria-current="page" href="../lemminkäinen/index.php">ETUSIVU</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="../lemminkäinen/eläimet.php">ELÄIMET</a>
@@ -25,15 +26,14 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
           <a class="nav-link" href="../lemminkäinen/PALAUTE.php">PALAUTE</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../lemminkäinen/oheistuotteet.php">OHEISTUOTTEET</a>
+          <a class="nav-link active" href="../lemminkäinen/oheistuotteet.php">OHEISTUOTTEET</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="../lemminkäinen/kamppanja.php">KAMPPANJAT</a>
           </li>
       </ul>
   </header>
-
-    <div class="container">
+      <div class="dogs">
         <?php
           $products = getDogProducts();
           $animal = getDog();
@@ -42,10 +42,13 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
             echo "<div>" . "<h2>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
             }
           foreach($products as $p){
-          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . '</div>'. "</br>";
+          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . " " .'<button>Osta</button>' . '</div>' . "</br>";
           }
+        ?>
+      </div>
 
-
+      <div class="cats">
+        <?php
           $products = getCatProducts();
           $animal = getCat();
 
@@ -53,10 +56,13 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
             echo "<div>" . "<h2>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
             }
           foreach($products as $p){
-          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . '</div>'. "</br>";
+          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . " " .'<button>Osta</button>' . '</div>'. "</br>";
           }
-
-
+        ?>
+      </div>
+      
+      <div class="turtles">
+        <?php
           $products = getTurtleProducts();
           $animal = getTurtle();
 
@@ -64,10 +70,13 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
             echo "<div>" . "<h2>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
             }
           foreach($products as $p){
-          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . '</div>'. "</br>";
+          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . " " .'<button>Osta</button>' . '</div>'. "</br>";
           }
+        ?>
+      </div>
 
-
+      <div class="birds">
+        <?php
           $products = getBirdProducts();
           $animal = getBird();
 
@@ -75,9 +84,13 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
             echo "<div>" . "<h2>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
             }
           foreach($products as $p){
-          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . '</div>'. "</br>";
+          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . " " .'<button>Osta</button>' . '</div>'. "</br>";
           }
+        ?>
+      </div>
 
+      <div class="sheeps">
+        <?php
           $products = getSheepProducts();
           $animal = getSheep();
 
@@ -85,9 +98,13 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
             echo "<div>" . "<h2>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
             }
           foreach($products as $p){
-          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . '</div>'. "</br>";
+          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . " " .'<button>Osta</button>' . '</div>' . "</br>";
           }
+        ?>
+      </div>
 
+      <div class="horses">
+        <?php
           $products = getHorseProducts();
           $animal = getHorse();
 
@@ -95,10 +112,11 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
             echo "<div>" . "<h2>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
             }
           foreach($products as $p){
-          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . '</div>'. "</br>";
+          echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . " " .'<button>Osta</button>' . '</div>'. "</br>";
           }
         ?>
-    </div>
+      </div>
+
 
   <footer>
     <div class="container">
