66323e1d83421cf1ef521ea36ed6935b3a802f65
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2022-04-18 12:37:00 +0300

    oheistuotteiden tyylittelyä

--- lemminkäinen/oheistuotteet.php
+++ lemminkäinen/oheistuotteet.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="oheistuotteet.css">    
+<link rel="stylesheet" href="../oheistuotteet.css">    
 
     <title>LEMMINKAINEN</title>
 </head>
@@ -35,7 +35,7 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
   </header>
   <div class="container">
   <div class="row">
-    <div class="col">
+    <div class="col-3">
       
     </div>
     <div class="col">
@@ -45,7 +45,7 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
             $animal = getDog();
 
             foreach($animal as $a){
-              echo "<div>" . "<h2>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
+              echo "<div>" . "<h2 id='koira'>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
               }
             foreach($products as $p){
             echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . " " . "</br>" .'<button>Osta</button>' . '</div>' . "</br>";
@@ -59,7 +59,7 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
             $animal = getCat();
 
             foreach($animal as $a){
-              echo "<div>" . "<h2>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
+              echo "<div>" . "<h2 id='kissa'>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
               }
             foreach($products as $p){
             echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . " ". "</br>" .'<button>Osta</button>' . '</div>'. "</br>";
@@ -73,7 +73,7 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
             $animal = getTurtle();
 
             foreach($animal as $a){
-              echo "<div>" . "<h2>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
+              echo "<div>" . "<h2 id='kilpikonna'>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
               }
             foreach($products as $p){
             echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . " ". "</br>" .'<button>Osta</button>' . '</div>'. "</br>";
@@ -87,7 +87,7 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
             $animal = getBird();
 
             foreach($animal as $a){
-              echo "<div>" . "<h2>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
+              echo "<div>" . "<h2 id='lintu'>" . $a["lajinimi"] . "</h2>" . '</div>'. "</br>";
               }
             foreach($products as $p){
             echo "<div>".$p["tuote"]." ".$p["hinta"]."€"."</br>".$p["info"] . " ". "</br>" .'<button>Osta</button>' . '</div>'. "</br>";
@@ -123,7 +123,7 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
           ?>
       </div>
     </div>
-    <div class="col">
+    <div class="col-3">
       
     </div>
   </div>
