becaa84e58dd4907926055e3aa56216d6d12a6cd
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2022-04-23 17:07:17 +0300

    vuokraus lisätty

--- lemminkäinen/oheistuotteet.php
+++ lemminkäinen/oheistuotteet.php
@@ -7,6 +7,7 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="../oheistuotteet.css">    
+<link rel="stylesheet" href="styles.css">
 
     <title>LEMMINKAINEN</title>
 </head>
@@ -30,6 +31,9 @@ http://localhost/Lemmikkivuokraamo-frontend/lemmink%C3%A4inen/oheistuotteet.php
         </li>
         <li class="nav-item">
             <a class="nav-link" href="../lemminkäinen/kamppanja.php">KAMPPANJAT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../lemminkäinen/vuokraus.php">VUOKRAUS</a>
           </li>
       </ul>
   </header>
---
+++ lemminkäinen/vuokraus.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>LEMMINKAINEN</title>
+</head>
+<body>
+  <header>
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="../lemminkäinen/index.html">ETUSIVU</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../lemminkäinen/eläimet.html">ELÄIMET</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../lemminkäinen/PALAUTE.html">PALAUTE</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../lemminkäinen/oheistuotteet.html">OHEISTUOTTEET</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="../lemminkäinen/kamppanja.html">KAMPPANJAT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="../lemminkäinen/vuokraus.php">VUOKRAUS</a>
+          </li>
+      </ul>
+  </header>
