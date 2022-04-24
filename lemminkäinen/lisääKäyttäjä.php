ea4ae12558ba1aff1bdd1e6628126ee9fcc93a6f
CaritaNi <93704532+CaritaNi@users.noreply.github.com>
2022-04-24 14:02:03 +0300

    Merge branch 'master' of https://github.com/JohanKivilomplo/Lemmikkivuokraamo-frontend

--- lemminkäinen/lisääKäyttäjä.php
+++ lemminkäinen/lisääKäyttäjä.php
@@ -13,7 +13,7 @@
   <header>
       <ul class="nav nav-tabs">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="index.php">ETUSIVU</a>
+          <a class="nav-link" aria-current="page" href="/index.php">ETUSIVU</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="../lemminkäinen/eläimet.php">ELÄIMET</a>
