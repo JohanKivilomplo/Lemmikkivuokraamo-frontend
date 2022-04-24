ad4f6f1f97c377cfec473fd7a6d54fccf0a7dc65
CaritaNi <93704532+CaritaNi@users.noreply.github.com>
2022-04-24 14:28:55 +0300

    NAVIN KORJAUSTA

--- lemminkäinen/eläimet.php
+++ lemminkäinen/eläimet.php
@@ -14,7 +14,7 @@
   <header>
       <ul class="nav nav-tabs">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/index.php">ETUSIVU</a>
+          <a class="nav-link" aria-current="page" href="../index.php">ETUSIVU</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" href="../lemminkäinen/eläimet.php">ELÄIMET</a>
