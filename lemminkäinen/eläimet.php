db4b09a57779547e75411e92161bbd2c737cfa92
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2022-04-24 15:03:30 +0300

    taas navin korjailua :D

--- lemminkäinen/eläimet.php
+++ lemminkäinen/eläimet.php
@@ -35,9 +35,6 @@
             <a class="nav-link active" href="../lemminkäinen/lisääKäyttäjä.php">LUO KÄYTTÄJÄTUNNUS</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="../lemminkäinen/kirjautuminen.php">KIRJAUDU SISÄÄN</a>
-          </li>
-          <li class="nav-item">
             <?php 
                 if(isset($_SESSION["username"])){
                     echo '<a class="nav-link bg-danger" href="../lemminkäinen/uloskirjautuminen.php">Log out</a>';
