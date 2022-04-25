c949abedd3b0360aee2ef92a079d673ca66750c1
LastJonicorn <joni.k.lampela@gmail.com>
2022-04-25 14:10:33 +0300

    etusivun kuvat toimii

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="lemminkäinen/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>LEMMINKAINEN</title>
 </head>
@@ -64,7 +64,7 @@
             </div>
           </div>
           <div id="profile" class="col-5">
-            <img src="../lemminkäinen/kuvat/sheep.jpg">
+            <img src="lemminkäinen/kuvat/sheep.jpg">
           </div>
         </div>
       </div>
