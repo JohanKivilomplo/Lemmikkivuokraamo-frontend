5cf7e1f73fba7639d05ce1d3d6601b8a5ba48799
Benjamin Madetoja <benjaminmadetoja@gmail.com>
2022-05-03 11:04:17 +0300

    Pari kamppanjaa lisätty

--- lemminkäinen/kamppanja.php
+++ lemminkäinen/kamppanja.php
@@ -11,7 +11,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>LEMMINKAINEN</title>
 </head>
+
 <body>
+<?php require MODULES_DIR.'functions.php'?>
   <header>
       <ul class="nav nav-tabs">
         <li class="nav-item">
@@ -50,28 +52,28 @@
       </ul>
   </header>
 
-  <div class="container">
-    <div class="row">
-      <div class="col">
+<div class="container">
+  <div class="row">
+  <div class="col">
         <img src="" alt="">
         <h2>Vuokrattava eläin</h2>
         <p>info</p>
         <p>kampanja aika</p>
-      </div>
-      <div class="col">
-        <img src="" alt="">
-        <h2>Vuokrattava eläin</h2>
-        <p>info</p>
-        <p>kampanja aika</p>
-      </div>
-      <div class="col">
-        <img src="" alt="">
-        <h2>Vuokrattava eläin</h2>
-        <p>info</p>
-        <p>kampanja aika</p>
-      </div>
-    </div>
   </div>
+  <div class="col">
+        <img src="" alt="">
+        <h2>Vuokrattava eläin</h2>
+        <p>info</p>
+        <p>kampanja aika</p>
+  </div>
+  <div class="col">
+        <img src="" alt="">
+        <h2>Vuokrattava eläin</h2>
+        <p>info</p>
+        <p>kampanja aika</p>
+  </div>
+  </div>
+</div>
 
   <footer>
   </footer>
